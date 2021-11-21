01707480ca50c4e6fdb2fab5cde47b223c520494
hassan khalil <hkhalil@student.1337.ma>
2021-11-21 07:26:39 +0100

    initial commit

--- get_next_line.c
+++ get_next_line.c
@@ -2,19 +2,29 @@
 
 char	*get_next_line(int fd)
 {
-	char	buf[BUF_SIZE + 1];
+	char	buf[BUF_SIZE];
 	char	*line;
 	size_t	ret;
 	int		i = 0;
 
-	buf = malloc(BUF_SIZE + 1);
-	ret = read(fd, buf, BUF_SIZE);
-	while (ret)
+	if (!count)
 	{
-		if (buff[i] = '\n')
-			break;
-		i++;
-		ret--;
+		buf = malloc(sizeof(char) * BUF_SIZE);
+		ret = read(fd, buf, BUF_SIZE);
+		if (!ret)
+			return (0);
 	}
-
+	while (buf[index] != EOF && index < ret)
+	{
+		if (buff[index] == '\n')
+			break;
+		index++;
+	}
+	line = malloc(sizeof(char) * (index + 2));
+	next_line = malloc(sizeof(char) * (ret - index + 1));
+	if (!line || !next_line)
+		return (0);
+	if (ret = index + 1 || buf)
+		index = 0;
+	return ((char *)ft_substr(buf, Count, i + 1));
 }
