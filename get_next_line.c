a9f99c755c51cb081c349558b9fe2f5c1da3984a
hassan khalil <hkhalil@student.1337.ma>
2021-11-22 01:13:16 +0100

    fix some basic cases

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/21 12:51:36 by hkhalil           #+#    #+#             */
-/*   Updated: 2021/11/21 19:01:36 by hkhalil          ###   ########.fr       */
+/*   Updated: 2021/11/22 01:10:38 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,33 +26,40 @@ char	*get_next_line(int fd)
 	line = NULL;
 	while (1)
 	{
-		if (ft_strlen(NEXT_LINE))
+		if (NEXT_LINE)
 		{
 			line = ft_strdup(NEXT_LINE);
-			free(NEXT_LINE);
+		    //free(NEXT_LINE);
+            //free(buf);
+            NEXT_LINE = NULL;
 		}
 		ret = read(fd, buf, BUFFER_SIZE);
-		buf[ret] = 0;
 		if (ret == -1)
 			return (0);
-		else if (ret == 0)
+        buf[ret] = 0;
+		if (ret == 0)
 			return (line);
 		i = 0;
 		while (buf[i] != '\n' && i < ret)
 			i++;
-		if (buf[i] == '\n' || buf[i] == EOF)
+		if (buf[i] == '\n')
 		{
-			if (buf[i] == '\n')
-			{
-				i++;
-				NEXT_LINE = malloc(sizeof(char) * (BUFFER_SIZE - i + 1));
-				NEXT_LINE = ft_substr(buf, i, BUFFER_SIZE - i + 1);
-			}
-			free(buf);
-			return (ft_strjoin(line, ft_substr(buf, 0, i + 1)));
+			i++;
+			NEXT_LINE = ft_substr(buf, i, BUFFER_SIZE - i + 1);
 		}
 		else if (!i)
 			line = ft_strjoin(line, ft_substr(buf, 0, i + 1));
 	}
 	return (0);
-}	
+}
+
+/*#include <fcntl.h>
+
+int main()
+{
+    //char buff[10];
+    int fd = open("get_next_line.c", O_RDWR);
+    //char *line = get_next_line(fd);
+    printf("%s", get_next_line(fd));
+    return (0);
+}*/
